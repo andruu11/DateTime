711f9d36351fa577d162b7447b180261c109eba4
jhass2andruu <jhass11_candia_valdez@outlook.com>
2017-11-20 04:23:01 -0400

    c

---
+++ TimePicker/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace TimePicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txb1 = new System.Windows.Forms.TextBox();
+            this.txb2 = new System.Windows.Forms.TextBox();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.lbl7 = new System.Windows.Forms.Label();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Segoe UI", 12.25F);
+            this.lbl1.Location = new System.Drawing.Point(201, 30);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(0, 23);
+            this.lbl1.TabIndex = 0;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(19, 31);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(143, 23);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "Obtener fecha actual";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbl1);
+            this.groupBox1.Controls.Add(this.btn1);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.groupBox1.Location = new System.Drawing.Point(35, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(365, 77);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lbl2);
+            this.groupBox2.Controls.Add(this.btn2);
+            this.groupBox2.Location = new System.Drawing.Point(35, 122);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(365, 78);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(19, 35);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(143, 23);
+            this.btn2.TabIndex = 0;
+            this.btn2.Text = "Adicionar dias";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Segoe UI", 12.25F);
+            this.lbl2.Location = new System.Drawing.Point(205, 35);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(0, 23);
+            this.lbl2.TabIndex = 1;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lbl3);
+            this.groupBox3.Controls.Add(this.btn3);
+            this.groupBox3.Location = new System.Drawing.Point(36, 232);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(364, 70);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(18, 29);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(143, 23);
+            this.btn3.TabIndex = 0;
+            this.btn3.Text = "Restar dias";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.Location = new System.Drawing.Point(205, 29);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(0, 21);
+            this.lbl3.TabIndex = 1;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.lbl5);
+            this.groupBox4.Controls.Add(this.lbl4);
+            this.groupBox4.Controls.Add(this.btn4);
+            this.groupBox4.Location = new System.Drawing.Point(448, 13);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(460, 76);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "groupBox4";
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(20, 29);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(143, 23);
+            this.btn4.TabIndex = 6;
+            this.btn4.Text = "obtener dia de la semana";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl4.Location = new System.Drawing.Point(194, 28);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(0, 21);
+            this.lbl4.TabIndex = 7;
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Font = new System.Drawing.Font("Segoe UI", 12.25F);
+            this.lbl5.Location = new System.Drawing.Point(320, 30);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(0, 23);
+            this.lbl5.TabIndex = 8;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.lbl6);
+            this.groupBox5.Controls.Add(this.txb2);
+            this.groupBox5.Controls.Add(this.txb1);
+            this.groupBox5.Controls.Add(this.button1);
+            this.groupBox5.Location = new System.Drawing.Point(448, 122);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(460, 78);
+            this.groupBox5.TabIndex = 6;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "groupBox5";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dias de un mes y un año";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // txb1
+            // 
+            this.txb1.Location = new System.Drawing.Point(198, 35);
+            this.txb1.Name = "txb1";
+            this.txb1.Size = new System.Drawing.Size(60, 20);
+            this.txb1.TabIndex = 1;
+            // 
+            // txb2
+            // 
+            this.txb2.Location = new System.Drawing.Point(292, 35);
+            this.txb2.Name = "txb2";
+            this.txb2.Size = new System.Drawing.Size(52, 20);
+            this.txb2.TabIndex = 2;
+            // 
+            // lbl6
+            // 
+            this.lbl6.AutoSize = true;
+            this.lbl6.Location = new System.Drawing.Point(380, 35);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(0, 13);
+            this.lbl6.TabIndex = 3;
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.btn5);
+            this.groupBox6.Controls.Add(this.lbl7);
+            this.groupBox6.Location = new System.Drawing.Point(448, 232);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(460, 70);
+            this.groupBox6.TabIndex = 7;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "groupBox6";
+            // 
+            // lbl7
+            // 
+            this.lbl7.AutoSize = true;
+            this.lbl7.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl7.Location = new System.Drawing.Point(15, 21);
+            this.lbl7.Name = "lbl7";
+            this.lbl7.Size = new System.Drawing.Size(0, 25);
+            this.lbl7.TabIndex = 0;
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(345, 25);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(109, 23);
+            this.btn5.TabIndex = 1;
+            this.btn5.Text = "Formato fecha";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(951, 385);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.TextBox txb2;
+        private System.Windows.Forms.TextBox txb1;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.Label lbl7;
+        private System.Windows.Forms.Button btn5;
+
+    }
+}
+
---
+++ TimePicker/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimePicker
+{
+    public partial class Form1 : Form
+    {
+        DateTime fecha_actual = DateTime.Now;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            
+            lbl1.Text = fecha_actual.ToString();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            DateTime fecha_adicionada = DateTime.Now.AddDays(100);
+            lbl2.Text = fecha_adicionada.ToString();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            DateTime fecha_restada = DateTime.Now.AddDays(-150);
+            lbl3.Text = fecha_restada.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lbl4.Text = fecha_actual.DayOfWeek.ToString();
+            lbl5.Text = fecha_actual.Month.ToString();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txb1.Text))
+            {
+                MessageBox.Show("sirve la validadad");
+            }
+            else
+            {
+                int a = Convert.ToInt32(txb1.Text);
+                int m = Convert.ToInt32(txb2.Text);
+                lbl6.Text = DateTime.DaysInMonth(a, m).ToString();
+            }
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            string formato = "ddd dd MMMM , yyyy // H:mm:ss ";
+            lbl7.Text = fecha_actual.ToString(formato);
+        }
+    }
+}
